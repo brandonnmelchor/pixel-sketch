5fd09d121d5955ceaec037ae6a4752ca4d7bef35
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-04-04 23:15:41 -0500

    create printGridSize, createDivs, and createGrid functions

--- javascript.js
+++ javascript.js
@@ -1,7 +1,28 @@
+const sketchpad = document.querySelector(".sketchpad");
 const gridSize = document.querySelector("#grid-size");
 const gridSizeOutput = document.querySelector("#grid-size-output");
-gridSize.addEventListener("input", printGridSize);
+gridSize.addEventListener("input", () => {
+  printGridSize(gridSize.value);
+  createDivs(gridSize.value);
+  createGrid(gridSize.value);
+});
 
-function printGridSize() {
-  gridSizeOutput.textContent = `${this.value} x ${this.value}`;
+function printGridSize(gridSize) {
+  gridSizeOutput.textContent = `${gridSize} x ${gridSize}`;
 }
+
+function createDivs(gridSize) {
+  sketchpad.innerHTML = "";
+  for (let i = 1; i <= Math.pow(gridSize, 2); i++) {
+    const div = document.createElement("div");
+    div.classList.add("pixel");
+    sketchpad.appendChild(div);
+  }
+}
+
+function createGrid(gridSize) {
+  sketchpad.style.gridTemplate = `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`;
+}
+
+createDivs(16);
+createGrid(16);
