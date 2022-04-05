12ee3944d0019b2a0e2c7830a4bc5b513eb93aef
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-04-05 11:48:37 -0500

    add vanta background, create newSketchpad(), add event listeners to div grids

--- javascript.js
+++ javascript.js
@@ -1,22 +1,33 @@
 const sketchpad = document.querySelector(".sketchpad");
 const gridSize = document.querySelector("#grid-size");
 const gridSizeOutput = document.querySelector("#grid-size-output");
+
+let isDrawing = false;
+
 gridSize.addEventListener("input", () => {
   printGridSize(gridSize.value);
-  createDivs(gridSize.value);
-  createGrid(gridSize.value);
+  newSketchpad(gridSize.value);
 });
 
 function printGridSize(gridSize) {
   gridSizeOutput.textContent = `${gridSize} x ${gridSize}`;
 }
 
+function newSketchpad(gridSize) {
+  createDivs(gridSize);
+  createGrid(gridSize);
+}
+
 function createDivs(gridSize) {
   sketchpad.innerHTML = "";
   for (let i = 1; i <= Math.pow(gridSize, 2); i++) {
     const div = document.createElement("div");
-    div.classList.add("pixel");
     sketchpad.appendChild(div);
+
+    div.classList.add("pixel");
+    div.addEventListener("mousedown", startDrawing);
+    div.addEventListener("mousemove", continueDrawing);
+    div.addEventListener("mouseup", () => (isDrawing = false));
   }
 }
 
@@ -24,5 +35,29 @@ function createGrid(gridSize) {
   sketchpad.style.gridTemplate = `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`;
 }
 
-createDivs(gridSize.value);
-createGrid(gridSize.value);
+function startDrawing() {
+  isDrawing = true;
+  this.classList.add("color-black");
+}
+
+function continueDrawing() {
+  if (isDrawing === true) {
+    this.classList.add("color-black");
+  }
+}
+
+newSketchpad(gridSize.value);
+
+VANTA.TRUNK({
+  el: document.querySelector("html"),
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x9bcac0,
+  backgroundColor: 0xfcfaf8,
+  spacing: 15.0,
+});
